a7c531e1e1efde07df4f24ae0ae0327546fa5182
Riley Moore <rilemoore@gmail.com>
2019-12-11 21:49:08 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,11 @@ ActiveRecord::Schema.define(version: 20151230232412) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
